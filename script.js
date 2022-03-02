b0ecb9958fa30e94d024206cb3e800708352a94e
Vannent <98601749+Vannent@users.noreply.github.com>
2022-03-01 19:05:55 -0600

    Numbers updated.

---
+++ script.js
@@ -0,0 +1,19 @@
+function addFunc(a, b) {
+    return a + b;
+}
+console.log("Add funct: " + addFunc(2, 4));
+
+function subFunc(a, b) {
+    return a - b;
+}
+console.log("Substract funct: " + subFunc(2, 4));
+
+function mulFunc(a, b) {
+    return a * b;
+}
+console.log("Multiply funct: " + mulFunc(2, 4));
+
+function divFunc(a, b) {
+    return a / b;
+}
+console.log("Divide funct: " + divFunc(2, 4));
