62e8d7e30da6858757e85330740f1f9bee49cdd3
Vannent <98601749+Vannent@users.noreply.github.com>
2022-03-03 16:50:38 -0600

    feat: add functionality to numbers and clear button.

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
-function addFunc(a, b) {
-    return a + b;
-}
-console.log("Add funct: " + addFunc(2, 4));
+let currentOperand = "";
+let previousOperand = "";
+let operation = undefined;
 
-function subFunc(a, b) {
-    return a - b;
-}
-console.log("Substract funct: " + subFunc(2, 4));
+const numberButtons = document.querySelectorAll("[data-numbers]");
+const operandButtons = document.querySelectorAll("[data-operators]");
+const clearButton = document.getElementById("clear");
+const deleteButton = document.getElementById("delete");
+const equalButton = document.getElementById("equals");
+const previousOperandText = document.getElementById("previous-operand");
+const currentOperandText = document.getElementById("current-operand");
 
-function mulFunc(a, b) {
-    return a * b;
-}
-console.log("Multiply funct: " + mulFunc(2, 4));
+numberButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        appendNumber(button.textContent);
+        console.log("Button " + e.target.textContent + " clicked.");
+    })
+})
 
-function divFunc(a, b) {
-    return a / b;
+clearButton.addEventListener("click", (e) => {
+    clear();
+    console.log("Button " + e.target.textContent + " clicked.");
+})
+
+function appendNumber(number) {
+    currentOperandText.textContent += number;
 }
-console.log("Divide funct: " + divFunc(2, 4));
+
+function clear() {
+    currentOperandText.textContent = "";
+    previousOperandText.textContent = "";
+    currentOperand = "";
+    previousOperand = "";
+    operation = undefined
+}
+// function addFunc(a, b) {
+//     return a + b;
+// }
+// console.log("Add funct: " + addFunc(2, 4));
+
+// function subFunc(a, b) {
+//     return a - b;
+// }
+// console.log("Substract funct: " + subFunc(2, 4));
+
+// function mulFunc(a, b) {
+//     return a * b;
+// }
+// console.log("Multiply funct: " + mulFunc(2, 4));
+
+// function divFunc(a, b) {
+//     return a / b;
+// }
+// console.log("Divide funct: " + divFunc(2, 4));
